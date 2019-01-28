a98fa41e1cf4d467d5bc8d7f7ccbc85d835795a6
ansaws <junk.ansaws@gmail.com>
2019-01-27 19:30:14 -0600

    Changed names to most recent

--- exercise_3.py
+++ exercise_3.py
@@ -1,15 +1,37 @@
 #sucess
-'''
-Take a list, say for example this one:
+my_list = []
 
-  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-and write a program that prints out all the elements of the list that are less than 5.
-'''
+while True:
+    my_list_len =len(my_list)
+    append = None
+    while append == None:
+        try:
+            append = input('Do you want to add a number to the list, press y for yes and press n no: ')
+        except:
+            print('Please input a y or n')
+    if append == "y":
+        while len(my_list) == my_list_len:
+            try:
+                my_list.append(int(input("Enter a number to input: ")))
+            except:
+                print("add a valid number to the list")
+    if append == "n":
+        break
+    print("These are the items in your list so far: ")
+    for item in my_list:
+        print(item)
+    
 
-my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
-my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
+append_list = []
 for item in my_list:
     if item<5:
+        append_list.append(item)
+
+
+
+if len(append_list)>0:
+    print("These are the items in your list that are under 5:")
+    for item in append_list:
         print(item)
+if len(append_list)== 0:
+    print("there are no items in your list")
